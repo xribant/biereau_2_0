a1d24a405c7b426a438d70b71dcefc1a2112dfad
Golgot <xribant@gmail.com>
2020-10-07 22:52:51 +0200

    Multi Changes

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -9,14 +12,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SubscriptionController extends AbstractController {
 
     /**
-     * @Route("/inscription", name="subscription_page")
+     * @Route("/inscription", name="subscription.index")
+     * @return Response
      */
-    public function showSubscriptionPage()
+    public function show(): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
         return $this->render('subscription.html.twig', [
-            'current_menu' => 'subscription',
+            'current_menu' => 'subscription'
         ]);
     }
+
+
 }
 
 
---
+++ src/Repository/ContactSubRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ContactSub;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method ContactSub|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ContactSub|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ContactSub[]    findAll()
+ * @method ContactSub[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ContactSubRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ContactSub::class);
+    }
+
+    // /**
+    //  * @return ContactSub[] Returns an array of ContactSub objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?ContactSub
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
